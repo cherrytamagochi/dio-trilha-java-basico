b741af162e07d8e465f1dcc3cb6101fa96ab4dc8
giovanna <giovannadacostalopes@gmail.com>
2024-06-24 14:18:34 -0300

    Projeto Iphone

---
+++ Diagrama-Iphone/Diagrama-Iphone/src/Iphone.java
@@ -0,0 +1,20 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    public void ligar() {}
+
+    public void atender() {}
+
+    public void iniciarCorreioVoz() {}
+
+    public void exibirPagina() {}
+
+    public void adicionarNovaAba() {}
+
+    public void atualizarPagina() {}
+
+    public void tocar() {}
+
+    public void pausar() {}
+
+    public void selecionarMusica() {}
+}
